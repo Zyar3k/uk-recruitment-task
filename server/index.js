a959364769ce2b15b00b7a832f97554ad504dc3e
Arkadiusz Zygan <a.zygan@gmail.com>
2021-05-21 19:57:06 +0200

    Connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Getting better... MongoDB Connected");
+  })
+  .catch((err) => console.log(err));
 
 app.listen(8800, () => {
   console.log("It's amazing! Server is running!");
